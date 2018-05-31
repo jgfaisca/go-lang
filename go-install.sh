f1f787319a60ea638bb7969e2949c33858ce2f48
zekaf <jose.faisca@gmail.com>
2018-05-31 18:31:43 +0100

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -51,7 +51,7 @@ mkdir -p $HOME/go
 mkdir -p $HOME/work
 
 # set environment
-cat $HOME/.bashrc | grep $INSTALL_PATH/go
+cat $HOME/.bashrc | grep $INSTALL_PATH/go &> /dev/null
 if [ $? -ne 0 ]; then
   echo "export PATH=\$PATH\:/usr/local/go/bin" >> $HOME/.bashrc
   echo "export GOPATH=\$HOME\/work" >> $HOME/.bashrc
